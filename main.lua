4457c4c913d4721d5313a23ebd13cac276de079f
++ <ionlyplusplus@gmail.com>
2024-10-17 03:57:26 +0100

    minor chanegs

--- main.lua
+++ main.lua
@@ -1,7 +1,10 @@
 
 Class = require 'class'
 push = require 'push'
+
 require 'objects/paddle'
+require 'objects/ball'
+
 
 Window_width = 640
 Window_height = 360
@@ -10,20 +13,20 @@ VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 PADDLE_SPEED = 200
 
-
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    
+    love.window.setTitle('Pong')
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, Window_width, Window_height, {
-            fullscreen = false,
-            resizable = false,
-            vsync = true
-        }
-    )
-    math.randomseed(os.time())
-    P1 = Paddle( 5 ,VIRTUAL_HEIGHT / 2 - 10, 5, 20)
-    P2 = Paddle( VIRTUAL_WIDTH - 10 ,VIRTUAL_HEIGHT / 2 - 10, 5, 20)
-    gamestate = 'start'
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    }
+)
+math.randomseed(os.time())
+P1 = Paddle( 5 ,VIRTUAL_HEIGHT / 2 - 10, 5, 20)
+P2 = Paddle( VIRTUAL_WIDTH - 10 ,VIRTUAL_HEIGHT / 2 - 10, 5, 20)
+B1 = Ball(VIRTUAL_WIDTH / 2 - 4, VIRTUAL_HEIGHT / 2 - 4, 4)
+gamestate = 'start'
 end
 
 
@@ -35,7 +38,7 @@ function love.update(dt)
     else
         P1.dy = 0
     end
-
+    
     if love.keyboard.isDown('up') then
         P2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
@@ -43,10 +46,13 @@ function love.update(dt)
     else
         P2.dy = 0
     end
+
     if gamestate == 'play' then
+        if B1:colision(P1) or B1:colision(P2) then
+            B1.dx = -B1.dx
+        end
         B1:update(dt)
     end
-    
     P1:update(dt)
     P2:update(dt)
 
@@ -56,9 +62,10 @@ function love.draw()
     
     push:apply('start')
     love.graphics.clear(r, g, b, a)
-    love.graphics.printf( 'hello word', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf( 'hello word', 0, VIRTUAL_HEIGHT / 5 - 6, VIRTUAL_WIDTH, 'center')
     P1:draw()
     P2:draw()
+    B1:draw()
     push:apply('end')
 end
 
@@ -66,5 +73,13 @@ end
 function love.keypressed(key)
     if (key == 'escape') then
         love.event.quit()
+    end
+    if (key == 'space') then
+        if gamestate == 'play' then
+            gamestate = 'start'
+        else
+            gamestate = 'play'
+            B1:reset()
+        end
     end
 end
--- objects/ball.lua
+++ objects/ball.lua
@@ -1,18 +1,51 @@
-Ball = class {}
+Ball = Class{}
 
 function Ball:init(x, y, R)
     self.x = x
     self.y = y
     self.R = R
-    self.dx =
-    self.dy = 
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy =  math.random(-50, 50)
+
 
 end
 
 function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
 
 end
 
-function Ball:render()
+function Ball:reset(dt)
+    self.x = VIRTUAL_WIDTH / 2 - 4
+    self.y = VIRTUAL_HEIGHT / 2 - 4
+    self.dx = math.random(2) == 1 and -100 or 100
+    self.dy =  math.random(-50, 50)
+end
 
+-- function Ball:colision(P)
+--     -- return (
+--     --     P.x > self.x + self.R or
+--     --     self.x > P.x + P.width or
+--     --     P.y > self.y + self.R or
+--     --     self.y > P.y + P.height
+--     -- )
+--     return not (
+--         self.x - self.R > P.x + P.width or  -- Ball is to the right of the paddle
+--         self.x + self.R < P.x or            -- Ball is to the left of the paddle
+--         self.y - self.R > P.y + P.height or -- Ball is below the paddle
+--         self.y + self.R < P.y               -- Ball is above the paddle
+--     )
+-- end
+function Ball:colision(P)
+    return not (
+        self.x - self.R > P.x + P.width or  -- Ball is to the right of the paddle
+        self.x + self.R < P.x or            -- Ball is to the left of the paddle
+        self.y - self.R > P.y + P.height or -- Ball is below the paddle
+        self.y + self.R < P.y               -- Ball is above the paddle
+    )
+end
+
+function Ball:draw()
+    love.graphics.circle('fill', self.x, self.y, self.R)
 end
